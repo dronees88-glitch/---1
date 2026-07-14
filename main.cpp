9ff94082d8a796f4b7956e0630839f0fb57ecc85
dronees88-glitch <dronees88@gmail.com>
2026-07-14 16:05:38 +0300

    Кейс-задача № 1
    Добавил

---
+++ main.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main() {
+    setlocale(LC_ALL, "rus");
+
+    int N;
+    cout << "Введите размер массива: ";
+    cin >> N;
+
+    int A[100];
+
+    cout << "Введите элементы массива:" << endl;
+    for (int i = 0; i < N; i++)
+    {
+        cin >> A[i];
+    }
+
+    int maxIndex = 0;
+    int minIndex = 0;
+
+    for (int i = 1; i < N; i++)
+    {
+        if (A[i] > A[maxIndex])
+            maxIndex = i;
+
+        if (A[i] < A[minIndex])
+            minIndex = i;
+    }
+
+    int max, min;
+
+    if (maxIndex < minIndex)
+    {
+        max = maxIndex;
+        min = minIndex;
+    }
+    else
+    {
+        max = minIndex;
+        min = maxIndex;
+    }
+
+    int sum = 0;
+
+    for (int i = max + 1; i < min; i++)
+    {
+        if (A[i] < 0)
+        {
+            sum += A[i];
+        }
+    }
+
+    cout << "Сумма отрицательных элементов между максимальным и минимальным: ";
+    cout << sum << endl;
+
+    return 0;
+}
